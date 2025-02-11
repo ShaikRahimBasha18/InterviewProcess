758f24079d3f6b4591b8f94db2b57cc140ad0533
GainSkills <mr.rahimbasha18@gmail.com>
2025-02-11 17:45:20 +0530

    Update GainSkills.js

--- GainSkills/GainSkills.js
+++ GainSkills/GainSkills.js
@@ -1,15 +1,70 @@
-let registerbtn=document.getElementById("register-link")
-let loginform=document.getElementById("login-form")
-let registerfrom=document.getElementById("register-form")
-let registerbtn2=document.getElementById("register-btn")
-registerbtn.addEventListener('click',()=>{
-  loginform.style.display="none"
-  registerfrom.style.display='block'
+// // let registerbtn=document.getElementById("register-link")
+// // let loginform=document.getElementById("login-form")
+// // let registerfrom=document.getElementById("register-form")
+// // let registerbtn2=document.getElementById("register-btn")
+let menubtn=document.getElementById("menubtn1")
+let menubtn2=document.getElementById("menubtn2")
+let menubar=document.getElementById("menubar1")
+let closebtn=document.getElementById("closebtn")
+// // registerbtn.addEventListener('click',()=>{
+// //   console.log("hello1")
+// //   loginform.style.display="none"
+// //   registerfrom.style.display='block'
+// // })
+// // registerbtn2.addEventListener('click',()=>{
+// //   loginform.style.display="block"
+// //   registerfrom.style.display='none'
+// // })
+menubtn.addEventListener("click",()=>{
+  console.log("hello")
+  menubar.style.display="block"
+  menubtn2.style.display="block"
+  menubtn.style.display="none"
+//  videodiv.style.height="50%"
 })
-registerbtn2.addEventListener('click',()=>{
-  loginform.style.display="block"
-  registerfrom.style.display='none'
+menubtn2.addEventListener("click",()=>{
+  console.log("hello")
+  menubar.style.display="none"
+  menubtn2.style.display="none"
+  menubtn.style.display="block"
 })
+
+
+// const imageContainer= document.querySelector('.image-container');
+// const slides = document.querySelectorAll('.slide');
+// const nextButton = document.getElementById('next-button');
+// const backButton = document.getElementById('back-button');
+// let currentSlide = 0;
+
+// slides[currentSlide].classList.add('active');
+
+// nextButton.addEventListener('click', () => {
+//   slides[currentSlide].classList.remove('active');
+//   currentSlide = (currentSlide + 1) % slides.length;
+//   slides[currentSlide].classList.add('active');
+// });
+
+// backButton.addEventListener('click', () => {
+//   slides[currentSlide].classList.remove('active');
+//   currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+//   slides[currentSlide].classList.add('active');
+// });
+// const video=document.getElementById("videoCon")
+// const videodiv=document.getElementById("videoDiv")
+//  videodiv.style.width="90%"
+
+// let registerbtn=document.getElementById("register-link")
+// let loginform=document.getElementById("login-form")
+// let registerfrom=document.getElementById("register-form")
+// let registerbtn2=document.getElementById("register-btn")
+// registerbtn.addEventListener('click',()=>{
+//   loginform.style.display="none"
+//   registerfrom.style.display='block'
+// })
+// registerbtn2.addEventListener('click',()=>{
+//   loginform.style.display="block"
+//   registerfrom.style.display='none'
+// })
 
 
 
